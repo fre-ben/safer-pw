fd8efc94aea0c184155b782fc5fb39ebc17a1d0b
fre-ben <fschocke@gmail.com>
2021-03-01 18:30:19 +0100

    ✨ Add password check prompts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,37 @@
-const [command] = process.argv.slice(2);
+import prompts from "prompts";
 
-console.log(command);
+const run = async () => {
+  console.log("Welcome to Safer-PW! 🔐");
+
+  const savePass = await prompts([
+    {
+      type: "text",
+      name: "save",
+      message: "Do you want to save a password? (yes/no)",
+    },
+    {
+      type: (prev) => (prev == "yes" ? "password" : null),
+      name: "password",
+      message: "Enter the password:",
+    },
+  ]);
+
+  if (savePass.save === "yes") {
+    const validatePass = await prompts({
+      type: "password",
+      name: "passwordValidate",
+      message: "Please re-enter password to validate:",
+    });
+    if (savePass.password === validatePass.passwordValidate) {
+      console.log("Password validated ✅");
+      console.log("Password saved 🔒");
+    } else if (savePass.password !== validatePass.passwordValidate) {
+      console.log("Passwords didn't match ❌");
+      console.log("Please restart, if you want to try again ↩");
+    }
+  } else {
+    console.log("Goodbye! Thank you for using Safer-PW 💖");
+  }
+};
+
+run();
