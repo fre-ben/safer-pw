95595252afcd1a8ef231a7a0baed5516454d098b
fre-ben <fschocke@gmail.com>
2021-03-02 15:59:16 +0100

    ✨ Connect to mongoDB

--- src/app.ts
+++ src/app.ts
@@ -6,22 +6,35 @@ import {
 import { printGoodbyeMessage, printWelcomeMessage } from "./messages";
 import { askSavePassword } from "./questions";
 
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
 const run = async () => {
-  printWelcomeMessage();
+  const url = process.env.MONGODB_URL;
 
-  const savePassword = await askSavePassword();
-
-  switch (savePassword.answer) {
-    case "yes":
-      const password = await handleEnterPassword();
-      const validationPassword = await handleValidatePassword();
-
-      handleCheckPassword(password.password, validationPassword.password);
-      break;
-    case "no":
-      printGoodbyeMessage();
-      break;
+  try {
+    const client = await MongoClient.connect(url, {
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB!");
+    client.close();
+  } catch (error) {
+    console.error(error);
   }
+
+  //   printWelcomeMessage();
+  //   const savePassword = await askSavePassword();
+  //   switch (savePassword.answer) {
+  //     case "yes":
+  //       const password = await handleEnterPassword();
+  //       const validationPassword = await handleValidatePassword();
+  //       handleCheckPassword(password.password, validationPassword.password);
+  //       break;
+  //     case "no":
+  //       printGoodbyeMessage();
+  //       break;
+  //   }
 };
 
 run();
